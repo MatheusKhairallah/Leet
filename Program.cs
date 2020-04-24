611c28074266f8c5a7f3d3fcfeaa5b28d75dfafa
MatheusKhairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2020-04-24 20:40:03 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Leet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A Etec Adolpho Berezin e muito legal!!!");
+            Console.WriteLine("Pressione enter para continuar...");
+            Console.ReadKey();
+           
+            Console.Clear();
+
+            Console.WriteLine("Leet traduzindo texto...");
+            Console.WriteLine("Pressione enter para continuar...");
+            Console.ReadKey();
+            
+            Console.Clear();
+
+            Console.WriteLine("4 373c 4d01ph0 B3r3z1n 3 mu170 13g41!!!");
+
+
+
+
+        }
+    }
+}
